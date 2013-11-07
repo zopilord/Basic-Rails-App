c901d0c2f3fe3323dd699038668489ebb83cc850
Uriel <urielcornel@gmail.com>
2013-11-07 17:43:09 -0600

    Associated users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
 	has_many :comments
-  attr_accessible :body, :title
+  	attr_accessible :body, :title
+  	belongs_to :user
+  	default_scope order('created_at DESC')
 end
